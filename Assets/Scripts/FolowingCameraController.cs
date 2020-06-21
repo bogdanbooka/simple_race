0477f64cd86a8f7fc284ae70c748d624623bc5bb
Melnikov <BMelnikov@SPB.luxoft.com>
2020-06-21 19:01:12 +0300

    Done: change folowing camera behavior

--- Assets/Scripts/FolowingCameraController.cs
+++ Assets/Scripts/FolowingCameraController.cs
@@ -1,24 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FolowingCameraController : MonoBehaviour
 {
-    public Camera followingCamera;
-    private Quaternion _cameraRot;
-    private Vector3 _cameraPos;
+    public float distanceToObject = 10;
+    public float heightAboveObject = 3.5f;
 
-    // Start is called before the first frame update
+    public Camera followingCamera;
+
     void Start()
     {
-        _cameraRot = followingCamera.transform.rotation;
-        _cameraPos = followingCamera.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        followingCamera.transform.rotation = _cameraRot;
-        followingCamera.transform.position = gameObject.transform.position + _cameraPos;
+        UpdateCameraPosition();
+        UpdateCameraRotation();
     }
+
+    private void UpdateCameraRotation()
+    {
+        var gameObjectPos = gameObject.transform.position;
+        var cameraPos = followingCamera.transform.position;
+
+        var gameObjectToCameraVector = gameObjectPos - cameraPos;
+
+        followingCamera.transform.rotation = Quaternion.LookRotation(gameObjectToCameraVector.normalized);
+    }
+
+    private void UpdateCameraPosition() 
+    {
+        var gameObjectPos = gameObject.transform.position;
+        gameObjectPos.y = 0;
+
+        var cameraPos = followingCamera.transform.position;
+        cameraPos.y = 0;
+
+
+        var gameObjectToCameraVector = gameObjectPos - cameraPos;
+        var curDistance = gameObjectToCameraVector.magnitude;
+
+        var followingCameraPosition = followingCamera.transform.position + gameObjectToCameraVector.normalized * (curDistance - distanceToObject);
+        followingCameraPosition.y = gameObject.transform.position.y + heightAboveObject;
+
+        followingCamera.transform.position = followingCameraPosition;
+    }
+
+
 }
