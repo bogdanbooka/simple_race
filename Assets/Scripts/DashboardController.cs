a4814a7f3600a5cc4b1cba7375fe42088491e701
Melnikov <BMelnikov@SPB.luxoft.com>
2020-09-19 19:03:49 +0300

    Done: add logic to display car's current speed and gear

---
+++ Assets/Scripts/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DashboardController : MonoBehaviour
+{
+
+    public Car car;
+
+    public Text SpeedText;
+    public Text GearText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpeedText.text = car.LongSpeed.ToString("F0");
+        GearText.text = car.Gear.ToString();
+    }
+}
