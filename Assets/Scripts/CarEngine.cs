d2983b86d8a2a3a9f8f5dc61055f1941bde86964
Melnikov <BMelnikov@SPB.luxoft.com>
2020-09-19 19:02:56 +0300

    Done: add engine sound logic

---
+++ Assets/Scripts/CarEngine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Car))]
+[RequireComponent(typeof(AudioSource))]
+public class CarEngine : MonoBehaviour
+{
+    Car car;
+    AudioSource audioSrc;
+    [SerializeField]
+    float PitchModifier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = GetComponent<Car>();
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float soundPitch = Mathf.Abs(car.Throttle) * PitchModifier + 0.5f;
+
+        if (car.Brake)
+        {
+            soundPitch = 0.5f;
+        }
+        else if (car.LongSpeed < 0)
+        {
+            soundPitch = Mathf.Abs(car.Throttle) * PitchModifier + 0.5f;
+        }
+        else {
+            soundPitch = Mathf.Abs(car.Throttle) * PitchModifier + 0.5f;
+        }
+
+
+        audioSrc.pitch = soundPitch;
+
+    }
+}
