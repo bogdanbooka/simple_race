d09474229d7db5edc14b93867e681ad779a4b918
Melnikov <BMelnikov@SPB.luxoft.com>
2020-09-21 19:58:24 +0300

    Done: add grasshopper and cricket sounds and logic

---
+++ Assets/Scripts/EnvironmentSoundController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Car))]
+public class EnvironmentSoundController : MonoBehaviour
+{
+    public Car car;
+    AudioSource audioSrc; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject != car.gameObject)
+        {
+            return;
+        }
+        if (audioSrc.isPlaying)
+        {
+            return;
+        }
+        audioSrc.Play();
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject != car.gameObject)
+        {
+            return;
+        }
+        audioSrc.Pause();
+    }
+}
