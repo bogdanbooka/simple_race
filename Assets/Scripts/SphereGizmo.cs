e8952434e589d73f44059f1c90ba5fa0c9686ac4
Melnikov <BMelnikov@SPB.luxoft.com>
2020-10-10 22:44:09 +0300

    Done: move sound prefabs to separate folder Done: add permanent sphere gizmos to sound prefabs

---
+++ Assets/Scripts/SphereGizmo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereGizmo : MonoBehaviour
+{
+    public bool showGizmos = false;
+    public Color color = Color.yellow;
+
+    void OnDrawGizmos()
+    {
+        if (showGizmos) 
+        {
+            // Draw a sphere at the transform's position
+            SphereCollider sphereCollider = GetComponent<SphereCollider>();
+
+            Gizmos.color = color;
+            Gizmos.DrawWireSphere(transform.position, sphereCollider.radius);
+        }
+    }
+}
